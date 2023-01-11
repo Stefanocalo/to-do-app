2303b4c408fcd0eee53ee3af89738fc935f8cc1c
Stefano Calo <stefano.cal92@gmail.com>
2023-01-11 17:40:30 +0100

    Add todo form

--- src/features/TodoForm/TodoForm.jsx
+++ src/features/TodoForm/TodoForm.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, {useState } from "react";
 import { useSelector } from "react-redux";
-import { Modal } from "../../style";
+import { Modal, ModalContainer, Close, FormContainer, theme } from "../../style";
+import {MdOutlineClose} from 'react-icons/md';
 
-export const TodoForm = () => {
+export const TodoForm = ({theme}) => {
 
     const todos = useSelector(state => state.todo.category);
 
-    
+    const [title, setTitle] = useState('');
+    const [status, setStatus] = useState('');
 
     return(
         <Modal>
+            <ModalContainer>
+                <div style={{display: 'flex', justifyContent: 'flex-end',  width: '100%'}}>
+                    <Close>
+                        <MdOutlineClose style={{fontSize: '1.3rem'}}/>
+                    </Close>
+                </div>
+                <FormContainer>
+                    <h1>Add Task</h1>
+                    <form>
+                        <label htmlFor="title">Title</label>
+                        <input type='text' id='title' value={title} onChange={(e) => setTitle(e.target.value)} ></input>
+                    </form>
+                </FormContainer>
+
+            </ModalContainer>
             
         </Modal>
     )
--- src/style.js
+++ src/style.js
@@ -7,6 +7,7 @@ export const theme = {
         secondary: '#404040',
         tertiary: '#214eb0',
         quaternary: '#686c75',
+        red: '#f2403d',
         buttonP: '#5e8df2',
         buttonS: '#80889c'
     }
@@ -39,6 +40,10 @@ export const ButtonP = styled.button`
     transition: 0.3s;
     :hover {
         background-color: ${(props) => props.theme.colors.tertiary};
+        scale: 1.2;
+    }
+    :active {
+        scale: 1;
     }
 `;
 
@@ -53,6 +58,13 @@ export const ButtonS = styled.button`
     border-radius: 8px;
     cursor: pointer;
     transition: 0.3s;
+    :hover {
+        background-color: ${(props) => props.theme.colors.red};
+        scale: 1.2;
+    }
+    :active {
+        scale: 1;
+    }
 `;
 
 export const Selection = styled.select`
@@ -68,7 +80,9 @@ export const Selection = styled.select`
     transition: 0.3s;
     :hover {
         background-color: ${(props) => props.theme.colors.quaternary};
+        scale: 1.2;
     }
+   
 `;
 
 export const OptionWrapper = styled.div`
@@ -79,10 +93,47 @@ export const OptionWrapper = styled.div`
 
 export const Modal = styled.div`
     position: fixed;
+    display:flex;
+    align-items: center;
+    justify-content: center;
     top: 0;
     left: 0;
     height: 100vh;
     width: 100vw;
     background-color: rgba(63, 0, 255, 0.1);
     backdrop-filter: blur(0.2rem);
+`;
+
+export const ModalContainer = styled.div`
+    
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Close = styled.div`
+    background-color: ${(props) => props.theme.colors.primary};
+    border-radius: 8px;
+    transition: 0.3s;
+    cursor: pointer;
+    height: 2rem;
+    width: 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    :hover {
+        background-color: ${(props) => props.theme.colors.red};
+        scale: 1.2;
+    }
+    :active {
+        scale: 1;
+    }
+`;
+
+export const FormContainer = styled.div`
+    background-color: ${(props) => props.theme.colors.primary};
+    height: 20rem;
+    width: 20rem;
+    margin: 1rem 0;
+    border-radius: 8px;
 `;
